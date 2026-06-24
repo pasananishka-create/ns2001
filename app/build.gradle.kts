7ad36193a4455aefeb87a40005f6974655abb12e
pasananishka <pasananishka@users.noreply.github.com>
2026-06-24 16:01:50 +0530

    Bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bajaj.ns200.telemetry"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 
     buildTypes {
